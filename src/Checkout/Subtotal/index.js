b40d152e9c8445090daac2d0815c7387d06f4e41
Tejoyasaswi-17 <tejoyasaswi1703@gmail.com>
2023-09-03 23:20:16 +0530

    removed unnecessary comments

--- src/Checkout/Subtotal/index.js
+++ src/Checkout/Subtotal/index.js
@@ -9,7 +9,6 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              {/* Part of the homework */}
               Subtotal (0 items): <strong>{value}</strong>
             </p>
             <small className={styles.subtotal_gift}>
@@ -18,7 +17,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={0}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
