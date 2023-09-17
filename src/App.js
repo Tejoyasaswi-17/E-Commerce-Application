52d5b7bd022723cc9ed2168a6aafbb3ce868bd5d
Tejoyasaswi-17 <tejoyasaswi1703@gmail.com>
2023-09-17 20:28:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [dispatch] = useStateValue();
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      console.log("authUser", authUser);
       if (authUser) {
         dispatch({
           type: "SET_USER",
